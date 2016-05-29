f641fc54e909dd4ae2a007af8564f02f91ed0303
Carturo8824 <cristy8824@gmail.com>
2016-05-29 14:58:14 -0500

    Agregando el activity de listado de mascatos y el de Detalles

---
+++ app/src/main/java/com/carturo8824/petagram/DetalleMascota.java
@@ -0,0 +1,16 @@
+package com.carturo8824.petagram;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Borregos123 on 29/05/2016.
+ */
+public class DetalleMascota extends AppCompatActivity{
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_mascota_detalle);
+    }
+}
