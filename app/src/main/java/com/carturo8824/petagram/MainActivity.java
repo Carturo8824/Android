ca5736133f69f6851554dc00bcae94d4bfa9bdc3
Carturo8824 <cristy8824@gmail.com>
2016-05-29 17:52:59 -0500

    Agregando un boton de accion circular

--- app/src/main/java/com/carturo8824/petagram/MainActivity.java
+++ app/src/main/java/com/carturo8824/petagram/MainActivity.java
@@ -1,7 +1,9 @@
 package com.carturo8824.petagram;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        agregarFab();
+    }
+
+    public void agregarFab(){
+        FloatingActionButton miFAB = (FloatingActionButton) findViewById(R.id.fabMiFab);
+        miFAB.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
