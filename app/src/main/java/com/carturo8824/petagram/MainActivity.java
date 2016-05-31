525f966dd63e0d9d72cd0e56fd880b83e3f79373
Carturo8824 <cristy8824@gmail.com>
2016-05-30 21:17:28 -0500

    Agregando Snackbar y CoordinatorLayout

--- app/src/main/java/com/carturo8824/petagram/MainActivity.java
+++ app/src/main/java/com/carturo8824/petagram/MainActivity.java
@@ -1,9 +1,12 @@
 package com.carturo8824.petagram;
 
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,7 +23,16 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-
+               // Toast.makeText(getBaseContext(),getResources().getString(R.string.mensaje), Toast.LENGTH_SHORT).show();
+                Snackbar.make(v,getResources().getString(R.string.mensaje),Snackbar.LENGTH_LONG)
+                        .setAction(getResources().getString(R.string.texto_accion), new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Log.i("SNACKBAR","Click en Snackbar");
+                            }
+                        })
+                        .setActionTextColor(getResources().getColor(R.color.colorPrimary))
+                        .show();
             }
         });
     }
